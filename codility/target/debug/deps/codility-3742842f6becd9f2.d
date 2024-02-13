40cc4aa2ecf5573e7e59f87c94590e9092bfc4f1
Yass <bargach.y@gmail.com>
2024-02-13 23:29:50 +0100

    rm target

--- codility/target/debug/deps/codility-3742842f6becd9f2.d
+++
@@ -1,5 +0,0 @@
-/home/thedevone/algorithms-hardcore-learning/codility/target/debug/deps/codility-3742842f6becd9f2.rmeta: src/main.rs
-
-/home/thedevone/algorithms-hardcore-learning/codility/target/debug/deps/codility-3742842f6becd9f2.d: src/main.rs
-
-src/main.rs:
--- codility/target/debug/deps/codility-8693414b4b1cf698.d
+++
@@ -1,5 +0,0 @@
-/home/thedevone/algorithms-hardcore-learning/codility/target/debug/deps/codility-8693414b4b1cf698.rmeta: src/main.rs
-
-/home/thedevone/algorithms-hardcore-learning/codility/target/debug/deps/codility-8693414b4b1cf698.d: src/main.rs
-
-src/main.rs:
