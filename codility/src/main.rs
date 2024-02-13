ddbeda7bd3fe04595d5272272503abd17a635fd8
Yass <bargach.y@gmail.com>
2024-02-13 23:26:04 +0100

    binarygap

--- codility/src/main.rs
+++ codility/src/main.rs
@@ -9,17 +9,25 @@ fn binarygap_solution(N: i32) -> Vec<i32> {
     let mut vec_gaps : Vec<i32> = Vec::new();
     let mut res = (0..32).map (|n| (N >> n) & 1);
     let mut counter = 0;
+    let mut bounded = false;
     for bit in res {
         if (bit & 1) == 0 {
             counter +=1; 
         }
         else {
-            vec_gaps.push(counter);
+            if bounded == true {
+                vec_gaps.push(counter);
+            }
+            bounded = true;
             counter = 0; 
-
         }
     }
-    vec_gaps
+    if vec_gaps.len() == 0 {
+        vec_gaps.push(0);
+        vec_gaps
+    } else {
+        vec_gaps
+    }
 }
 
 
@@ -27,7 +35,7 @@ fn main() {
     /**
      * binary gap test
      */
-    let gaps = binarygap_solution(1041);
+    let gaps = binarygap_solution(32);
     println!("{:?}", gaps);
     let maxGap = gaps.iter().max();
     println!("{:?}", maxGap.unwrap());
