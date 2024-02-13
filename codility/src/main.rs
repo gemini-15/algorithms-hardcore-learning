1ece4a776260d951003f465706cfad6e2005b98a
Yass <bargach.y@gmail.com>
2024-02-14 00:28:14 +0100

    cyclic rotation

--- codility/src/main.rs
+++ codility/src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 
 
 
@@ -7,7 +9,7 @@
 fn binarygap_solution(N: i32) -> Vec<i32> {
     // interating through the bits of N
     let mut vec_gaps : Vec<i32> = Vec::new();
-    let mut res = (0..32).map (|n| (N >> n) & 1);
+    let res = (0..32).rev().map (|n| (N >> n) & 1);
     let mut counter = 0;
     let mut bounded = false;
     for bit in res {
@@ -30,13 +32,41 @@ fn binarygap_solution(N: i32) -> Vec<i32> {
     }
 }
 
+fn rotation_by_one(vector: Vec<i32>) -> Vec<i32> {
+    let vector_length = vector.len();
+    let mut new_vector: Vec<i32> = Vec::new();
+    new_vector.push(vector[vector_length-1]);
+    for index_element in 0..vector_length-1 {
+        if index_element+1 < vector_length {
+            new_vector.push(vector[index_element])
+        }
+    }
+    new_vector
+}
+
+fn cyclic_rotation_solution(vector: Vec<i32>, rotations: i32) -> Vec<i32> {
+    let mut cyclic_rot = vector;
+    for rotation in 0..rotations {
+        cyclic_rot = rotation_by_one(cyclic_rot);
+        println!("rotation {:?}, is : {:?}", rotation, cyclic_rot);
+    }
+    cyclic_rot
+}
 
 fn main() {
-    /**
+    /*
      * binary gap test
      */
-    let gaps = binarygap_solution(32);
-    println!("{:?}", gaps);
-    let maxGap = gaps.iter().max();
-    println!("{:?}", maxGap.unwrap());
+    // let gaps = binarygap_solution(32);
+    // println!("{:?}", gaps);
+    // let max_gap = gaps.iter().max();
+    // println!("{:?}", max_gap.unwrap());
+
+    /* 
+        Cyclic rotation test
+     */
+    // let test_vector = vec![0, 0, 0];
+    // let test_rotation = 3;
+    // let cyclic_rot = cyclic_rotation_solution(test_vector, test_rotation);
+    // println!("{:?}", cyclic_rot);
 }
