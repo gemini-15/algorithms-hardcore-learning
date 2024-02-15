2009ec80f3c43840d167702cb1a0b013ae891e5e
Yass <bargach.y@gmail.com>
2024-02-15 20:57:23 +0100

    step 3 & 4

--- codility/src/main.rs
+++ codility/src/main.rs
@@ -1,4 +1,5 @@
-use std;
+
+use std::{self, vec};
 use num_integer;
 
 /**
@@ -85,6 +86,130 @@ fn frog_jmp_solution(X: i32, Y: i32, D: i32) -> i32 {
     }
 }
 
+/**
+ * perm_missing_Elem
+ */
+fn perm_missing_elem_solution(vector: Vec<u16>) -> u16 {
+    let max_element: u16 = (vector.len() +1) as u16;
+    let combination = max_element * (max_element+1)/2;
+    let sum_all_elements:u16 = vector.iter().sum();
+    let missing = combination - sum_all_elements;
+    missing
+}
+
+
+/**
+ * tape_equilibrium
+ */
+fn tape_equilibrium(vector: Vec<u16>) -> u16 {
+    let mut min_absolute_diff: u16 = vector.len() as u16; 
+    for p in 0..(vector.len() as u16) {
+        let mut vector_clone = vector.clone();
+        let sec_vec = vector_clone.split_off(p.into()); 
+        let sum1:u16 = sec_vec.iter().sum();
+        let sum2:u16 = vector_clone.iter().sum();
+        let absolut_diff = sum1.abs_diff(sum2);
+        if absolut_diff < min_absolute_diff {
+            min_absolute_diff = absolut_diff;
+        }
+
+   }
+   min_absolute_diff
+}
+
+/**
+ * frogRiverOne
+ */
+fn frog_river_one(X: u16, vector: Vec<u16>) -> i16 {
+    use std::collections::HashSet;
+    let time_u16 = vector.len() as u16;
+    println!("{:?}", time_u16);
+    let mut t: u16 = 0;
+    let mut positions_known:HashSet<u16> = HashSet::new();
+    while positions_known.len() == X.into() || t < time_u16-2 {
+        positions_known.insert(vector[t as usize]);
+        t +=1;
+    }   
+    println!("{positions_known:?}");
+    if t >= time_u16-1 {
+        -1
+    }
+    else {
+        t.try_into().unwrap()
+    }
+}
+
+/**
+ * PermCheck
+ */
+fn permcheck_solution(vector: Vec<u16>) -> u8 {
+    let length_vector = vector.len();
+    let mut combination = (length_vector+1)*length_vector/2;
+    let mut sum_vector = 0;
+    for element in vector.clone() {
+        sum_vector += element;
+    }
+    if (combination as u16) == sum_vector {
+        1
+    }
+    else {
+        0
+    }
+}
+
+/**
+ * maxcounters
+ */
+fn increase_one_index(index: i32, vector: Vec<i32>) -> Vec<i32> {
+    let mut vector_increased = vector.clone();
+    vector_increased[index as usize] +=1; 
+    vector_increased
+}
+
+fn max_counters(vector: Vec<i32>) -> Vec<i32> {
+    let max_counter = vector.iter().max().unwrap();
+    vec![*max_counter; vector.len()]
+}
+
+fn max_counters_solution(N: i32, vector: Vec<i32>) -> Vec<i32> {
+    let mut counters: Vec<i32> = vec![0; N as usize]; //init N counters to 0 
+    for element in vector {
+        if element > N {
+            counters = max_counters(counters.clone());
+        }
+        else {
+            counters = increase_one_index(element-1, counters.clone())
+        }
+        println!("{:?}", counters);
+    }
+    counters
+} 
+
+
+/**
+ * Missing integer
+ */
+fn missing_integer(vector: &Vec<i32>)  -> i32 {
+    use std::collections::HashSet;
+
+    let mut numbers = HashSet::new();
+
+    // Removing all the negative numbers and 0
+    for &number in vector.iter() {
+        if number > 0 {
+            numbers.insert(number);
+        }
+    }
+
+    let mut missing_number = 1;
+
+    while numbers.contains(&missing_number)  {
+        missing_number +=1;
+    }
+
+    missing_number
+}
+
 fn main() {
     /*
      * binary gap test
@@ -112,7 +237,49 @@ fn main() {
         Frog jump
 
      */
-    println!("{:?}", frog_jmp_solution(120, 10, 30));
+    // println!("{:?}", frog_jmp_solution(120, 10, 30));
+
+
+    /*
+        PermMissingElem
+     */
+    // let A = vec![2, 3, 1, 5];
+    // println!("{:?}", perm_missing_elem_solution(A));
+
+
+    /*
+        TapeEquilibrum
+     */
+
+    //  let A = vec![3, 1, 2, 4, 3];
+    //  println!("{:?}", tape_equilibrium(A));
+
+
+     /*
+        Frog river one
+      */
+
+    // let A = vec![1, 3, 1, 4, 2, 3, 5, 4];
+    // println!("{:?}",frog_river_one(5, A));
+
+    /*
+        Permcheck
+     */
+    // let A = vec![4, 1, 3, 2];
+    // println!("{:?}",permcheck_solution(A));
+
+    /*
+        Max counters
+     */
+    // let A = vec![3, 4, 4, 6, 1, 4, 4];
+    // println!("{:?}", max_counters_solution(5, A));
+
+    /*
+        Smallest missing 
+     */
+    let A = vec![-1, -3];
+    println!("{:?}", missing_integer(&A));
+
 }
 
 
--- rust_notes.md
+++ rust_notes.md
@@ -4,3 +4,9 @@
 
 - `(x >> n) & 1` retrieves the `n`th bit 
 - `()
+
+## Array operations
+- `let v: Vec<i32> = vec![1; N];` initializes a vector of 1 and length N  
+
+## Structures usage
+- Hashset: [Hashset doc](https://doc.rust-lang.org/std/collections/struct.HashSet.html). Hashset can be used similarly to the `set()` function in python. Defining the unicity of an array. 
